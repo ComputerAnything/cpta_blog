c0c187254c988d24486aaafd62da3d4ee18ca936
dmurchison <duncancmurchison@gmail.com>
2025-04-02 01:10:16 -0400

    refactor import statements in routes.py for clarity

--- blog-posting-app/backend/routes.py
+++ blog-posting-app/backend/routes.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, request, jsonify
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity
-from models import User, BlogPost, db
+from app import db
+from models import User, BlogPost
 from flask_sqlalchemy import SQLAlchemy
 
 
