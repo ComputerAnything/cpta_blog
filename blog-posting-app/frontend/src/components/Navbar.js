2848419b7bec79ab609a80cb9474c4a2ad2d1ec6
dmurchison <duncancmurchison@gmail.com>
2025-04-21 17:49:15 -0400

    fixing Navbar

--- blog-posting-app/frontend/src/components/Navbar.js
+++ blog-posting-app/frontend/src/components/Navbar.js
@@ -5,40 +5,54 @@ const Navbar = ({ user, onLogout }) => {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark sticky-top px-4">
       <div className="container-fluid">
+        {/* Navbar brand */}
+        <a className="navbar-brand" href="/#home">
+          Computer Anything
+        </a>
 
-        {/* Navbar links */}
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav me-auto mb-2 mb-lg-0"> {/* Changed ms-auto to me-auto */}
+        {/* Navbar toggler for mobile */}
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        {/* Navbar content */}
+        <div className="collapse navbar-collapse" id="navbarNav">
+          {/* Left side: Links */}
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <a className="nav-link navbar-brand" href="/#home">Home</a>
+              <a className="nav-link" href="/#features">Features</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link navbar-brand" href="/#features">Features</a>
+              <a className="nav-link" href="/#contact">Contact</a>
             </li>
             <li className="nav-item">
-              <a className="nav-link navbar-brand" href="/#contact">Contact</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link navbar-brand" href="/posts">Blog</a>
+              <a className="nav-link" href="/posts">Blog</a>
             </li>
           </ul>
+
+          {/* Right side: Signed in message and logout button */}
+          {user && (
+            <div className="d-flex align-items-center">
+              <span className="navbar-text text-white me-3">
+                Signed in as: <strong>{user.username}</strong>
+              </span>
+              <button
+                className="btn btn-outline-light btn-sm"
+                onClick={onLogout}
+              >
+                Logout
+              </button>
+            </div>
+          )}
         </div>
-
-        {/* Right side: Signed in message and logout button */}
-        {user && user.username && (
-          <div className="d-flex align-items-center">
-            <span className="navbar-text text-white me-3">
-              Signed in as: <strong>{user.username}</strong>
-            </span>
-            <button
-              className="btn btn-outline-light btn-sm"
-              onClick={onLogout}
-            >
-              Logout
-            </button>
-          </div>
-        )}
-
       </div>
     </nav>
   );
