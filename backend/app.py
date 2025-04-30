7495fff2e3cdef4351208fd19bb95d191ba5e87c
dmurchison <duncancmurchison@gmail.com>
2025-04-29 20:55:35 -0400

    fix: Update import path for Config in app.py

--- backend/app.py
+++ backend/app.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_jwt_extended import JWTManager
 from flask_cors import CORS
-from backend.config import Config
+from config import Config
 from flask_mail import Mail
 
 # Absolute path to React build directory
